684cb8bf218053397d4c59f8f0a36a5ec21566f1
xuemingqi <mingqi.xue@net263.com>
2023-12-01 14:43:56 +0800

    [Feat]: log

--- x-work/src/test/java/com/x/work/test/ProducerTest.java
+++ x-work/src/test/java/com/x/work/test/ProducerTest.java
@@ -7,10 +7,17 @@ import com.x.websocket.dto.MessageDto;
 import com.x.work.WorkApplication;
 import com.x.work.mq.constants.TopicConstant;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author : xuemingqi
@@ -45,6 +52,45 @@ public class ProducerTest {
     public void test2() {
         MessageDto<String> messageDto = MessageDto.<String>builder().state(1).data("test message").build();
         webSocketApi.pub(123456L, messageDto);
+    }
+
+    @Test
+    public void push() {
+        try {
+            //项目路径
+            Path currentRelativePath = Paths.get("").toAbsolutePath().getParent();
+            String projectRootPath = currentRelativePath.toString();
+            log.info("path:{}", projectRootPath);
+
+            while (true) {
+                ProcessBuilder processBuilder = new ProcessBuilder("git", "push");
+                processBuilder.directory(new java.io.File(projectRootPath));
+                Process process = processBuilder.start();
+                int exitCode = process.waitFor();
+
+                //处理命令执行结果
+                if (exitCode == 0) {
+                    log.info("Git push successful");
+                    break;
+                } else {
+                    log.info("Git push failed. Retrying...");
+                    //读取错误信息
+                    printStream(process.getErrorStream());
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            log.error(ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    //打印输入流的内容
+    private void printStream(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
     }
 
     @BeforeAll
